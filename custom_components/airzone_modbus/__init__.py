4b8aad5574479b52e27088d227e203598f40b233
exelsis423 <119899103+exelsis423@users.noreply.github.com>
2026-08-27 09:51:05 +0200

    Implement async_setup for Airzone Modbus
    Add async_setup function for Airzone Modbus integration.

--- custom_components/airzone_modbus/__init__.py
+++ custom_components/airzone_modbus/__init__.py
@@ -1,1 +1,16 @@
+"""Intégration Airzone Modbus."""
 
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    """Configure l'intégration Airzone Modbus."""
+
+    hass.data.setdefault(DOMAIN, {})
+
+    return True
