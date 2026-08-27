6db7b452fe45e6b29fc4d8f8af76513d57313e79
exelsis423 <119899103+exelsis423@users.noreply.github.com>
2026-08-27 21:16:40 +0200

    Implement entry setup and unload for Airzone Modbus
    Added async_setup_entry and async_unload_entry functions for managing Airzone Modbus entries.

--- custom_components/airzone_modbus/__init__.py
+++ custom_components/airzone_modbus/__init__.py
@@ -1,5 +1,6 @@
 """Intégration Airzone Modbus."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
@@ -12,5 +13,28 @@ async def async_setup(
     """Configure l'intégration Airzone Modbus."""
 
     hass.data.setdefault(DOMAIN, {})
+
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Configure une entrée Airzone Modbus."""
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Décharge une entrée Airzone Modbus."""
+
+    hass.data[DOMAIN].pop(entry.entry_id, None)
 
     return True
